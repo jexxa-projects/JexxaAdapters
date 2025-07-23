ebca3a834f763fb6269afcc37834557b91385cdb
repplix <116941865+repplix@users.noreply.github.com>
2025-07-23 17:48:38 +0200

    * disabled connection sharing by default because a native jdbc connection is not thread safe. Therefore, you can only enable connection sharing in cause you use only a single thread. This is for example possible in Jexxa-applications.
    All other applications (such as JLegMed) cannot use this feature.
    
    * Added support methods to validate connection parameters

--- common-adapters/src/main/java/io/jexxa/common/facade/jdbc/JDBCConnectionPool.java
+++ common-adapters/src/main/java/io/jexxa/common/facade/jdbc/JDBCConnectionPool.java
@@ -6,16 +6,23 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import static io.jexxa.adapterapi.JexxaContext.registerCleanupHandler;
 
-@SuppressWarnings("java:S6548")
+@SuppressWarnings({"java:S6548", "unused"})
 public final class JDBCConnectionPool implements AutoCloseable {
 
     private static final JDBCConnectionPool JDBC_CONNECTION_POOL = new JDBCConnectionPool();
+    private static boolean isConnectionSharingEnabled = false;
 
     // Concurrent maps are used to handle optimized concurrent access to this class without synchronizing each method
     // This is possible because methods that are accessed in parallel are synchronized by the maps (such as computeIfAbsent)
     private final Map<String, JDBCConnection> sharedConnectionMap = new ConcurrentHashMap<>();
     private final Map<Object, JDBCConnection> exclusiveConnectionMap = new ConcurrentHashMap<>();
     private final Map<Object, JDBCConnection.IsolationLevel> connectionConfiguration = new ConcurrentHashMap<>();
+
+
+    public static void disableConnectionSharing() { isConnectionSharingEnabled = false; }
+
+    // This is only meaningful if your application runs only a single thread
+    public static void enableConnectionSharing() { isConnectionSharingEnabled = true; }
 
     public static synchronized JDBCConnection getJDBCConnection(Properties properties, Object managingObject)
     {
@@ -26,13 +33,20 @@ public final class JDBCConnectionPool implements AutoCloseable {
             throw new IllegalArgumentException("Parameter " + JDBCProperties.jdbcUrl() + " is missing");
         }
 
-        if (JDBC_CONNECTION_POOL.requiresExclusiveConnection(managingObject))
+        if (!isConnectionSharingEnabled || JDBC_CONNECTION_POOL.requiresExclusiveConnection(managingObject))
         {
             return JDBC_CONNECTION_POOL.getExclusiveConnection(properties, managingObject);
         }
         return JDBC_CONNECTION_POOL.getSharedConnection(properties, connectionName);
     }
 
+    public static boolean validateJDBCConnection(Properties properties)
+    {
+        try(var connection = new JDBCConnection(properties))
+        {
+            return connection.isValid();
+        }
+    }
 
     public static void configureExclusiveConnection(Object managingObject, JDBCConnection.IsolationLevel isolationLevel)
     {
