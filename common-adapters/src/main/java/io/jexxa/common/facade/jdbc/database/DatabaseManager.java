3deb75c33caceaf1bc23012226daf84937fb51b0
repplix <116941865+repplix@users.noreply.github.com>
2025-08-27 09:54:08 +0200

    added a null check on connectionURL

--- common-adapters/src/main/java/io/jexxa/common/facade/jdbc/database/DatabaseManager.java
+++ common-adapters/src/main/java/io/jexxa/common/facade/jdbc/database/DatabaseManager.java
@@ -6,6 +6,11 @@ public final class DatabaseManager
 {
     public static IDatabase getDatabase(String connectionURL)
     {
+        if (connectionURL == null )
+        {
+            throw new IllegalArgumentException("Connection URL is null");
+        }
+
         if ( connectionURL.toLowerCase(Locale.ENGLISH).contains("postgres") )
         {
             return new PostgresDatabase(connectionURL);
